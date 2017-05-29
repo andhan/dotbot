83bea6ab97d8b70fef9720561378fe9efec40501
Anders Hansson <anders.hansson@so4it.com>
2017-05-29 18:36:48 +0200

    Added ll to all windows

--- bin/nextgen-tmux.sh
+++ bin/nextgen-tmux.sh
@@ -24,40 +24,52 @@ then
 
 	#build window with promts at root and pu folders
 	tmux send-keys -t $SESSION_NAME 'cd '$NEXTGEN_HOME C-m
+	tmux send-keys -t $SESSION_NAME 'll' C-m
 	tmux split-window -h -t $SESSION_NAME
 	tmux send-keys -t $SESSION_NAME 'cd '$NEXTGEN_HOME'/nextgen-pu/' C-m
+	tmux send-keys -t $SESSION_NAME 'll' C-m
 	tmux split-window -v -t $SESSION_NAME:1
 	tmux send-keys -t $SESSION_NAME 'cd '$NEXTGEN_HOME'/nextgen-market/nextgen-market-pu' C-m
+	tmux send-keys -t $SESSION_NAME 'll' C-m
 
 	#test window with promts at root, integration and feature tests
 	tmux new-window -n test -t $SESSION_NAME
 	tmux select-window -t $SESSION_NAME:2
 	tmux send-keys -t $SESSION_NAME 'cd '$NEXTGEN_HOME C-m
+	tmux send-keys -t $SESSION_NAME 'll' C-m
 	tmux split-window -v -t $SESSION_NAME
 	tmux send-keys -t $SESSION_NAME 'cd '$NEXTGEN_HOME'/nextgen-integration-test' C-m
+	tmux send-keys -t $SESSION_NAME 'll' C-m
 	tmux split-window -v -t $SESSION_NAME:2.1
 	tmux send-keys -t $SESSION_NAME 'cd '$NEXTGEN_HOME'/nextgen-feature-test' C-m
+	tmux send-keys -t $SESSION_NAME 'll' C-m
 
 	#deploy window with promts at nextgen-pu-deploy and nextgen-marketpu-deploy
 	tmux new-window -n deploy -t $SESSION_NAME
 	tmux select-window -t $SESSION_NAME:3
 	tmux send-keys -t $SESSION_NAME 'cd '$NEXTGEN_HOME'/nextgen-pu/nextgen-pu-deploy' C-m
+	tmux send-keys -t $SESSION_NAME 'll' C-m
 	tmux split-window -h -t $SESSION_NAME:3
 	tmux send-keys -t $SESSION_NAME 'cd '$NEXTGEN_HOME'/nextgen-market/nextgen-market-pu/nextgen-market-pu-deploy' C-m
+	tmux send-keys -t $SESSION_NAME 'll' C-m
 
 	#load window with prompts at nextgen-jmeter, nextgen-jmeter-scenarios
 	tmux new-window -n load -t $SESSION_NAME
 	tmux select-window -t $SESSION_NAME:4
 	tmux send-keys -t $SESSION_NAME 'cd '$NEXTGEN_HOME'/nextgen-jmeter-test' C-m
+	tmux send-keys -t $SESSION_NAME 'll' C-m
 	tmux split-window -h -t $SESSION_NAME:4
 	tmux send-keys -t $SESSION_NAME 'cd '$NEXTGEN_HOME'/nextgen-jmeter-test/nextgen-jmeter-scenarios' C-m
+	tmux send-keys -t $SESSION_NAME 'll' C-m
 
 	#release window with promts at root, nextgen-doc 
 	tmux new-window -n release -t $SESSION_NAME
 	tmux select-window -t $SESSION_NAME:5
 	tmux send-keys -t $SESSION_NAME 'cd '$NEXTGEN_HOME C-m
+	tmux send-keys -t $SESSION_NAME 'll' C-m
 	tmux split-window -h -t $SESSION_NAME:5
 	tmux send-keys -t $SESSION_NAME 'cd '$NEXTGEN_HOME'/nextgen-doc' C-m
+	tmux send-keys -t $SESSION_NAME 'll' C-m
 
 
 	tmux select-window -t $SESSION_NAME:1
